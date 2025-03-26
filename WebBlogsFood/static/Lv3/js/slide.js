ac817eae0c6655326af6774fea3347bdbf5e98bf
MinhLysA <lle88862@gmail.com>
2025-03-26 20:46:31 +0700

    WebBlogsFood

---
+++ WebBlogsFood/static/Lv3/js/slide.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelector(".slides");
+    const slideItems = document.querySelectorAll(".slide");
+    const totalSlides = slideItems.length; // Số lượng slide
+    let currentIndex = 0;
+  
+    function updateSlide() {
+      slides.style.transform = `translateX(-${currentIndex * 100}%)`; // Mỗi lần di chuyển 100% vì mỗi slide chiếm toàn bộ chiều rộng
+    }
+  
+    document.querySelector(".next").addEventListener("click", function () {
+      currentIndex = (currentIndex + 1) % totalSlides;
+      updateSlide();
+    });
+  
+    document.querySelector(".prev").addEventListener("click", function () {
+      currentIndex = (currentIndex - 1 + totalSlides) % totalSlides;
+      updateSlide();
+    });
+  });
+  
